bc88d2ce89463e185fee725e0150379e04a70223
Max Golovnia <mgolovnia@gmail.com>
2011-11-11 14:38:27 +0200

    Refactoring blog api

--- lib/tumblr/client/blog.rb
+++ lib/tumblr/client/blog.rb
@@ -5,105 +5,95 @@ module Tumblr
   class Client
     module Blog
        
-      def blog_info(params={})
-        check_required_params %w(base-hostname), params
+      def blog_info(base_hostname, params={})
         params.merge!("api_key" => consumer_key)
-        get build_query("/#{API_VERSION}/blog/#{params.delete("base-hostname")}/info", params) 
+        get build_query("/#{API_VERSION}/blog/#{base_hostname}/info", params) 
       end
 
-      def avatar(params={})
-        check_required_params %w(base-hostname), params
-        get "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/avatar/#{params.delete("size")}", 
+      def avatar(base_hostname, params={})
+        get "/#{API_VERSION}/blog/#{base_hostname}/avatar/#{params.delete("size")}", 
       end
 
-      def followers(params={})
-        check_required_params %w(base-hostname), params
-        get build_query("/#{API_VERSION}/blog/#{params.delete("base-hostname")}/followers", params)
+      def followers(base_hostname, params={})
+        get build_query("/#{API_VERSION}/blog/#{base_hostname}/followers", params)
       end
 
-      def posts(params={})
-        check_required_params %w(base-hostname), params
+      def posts(base_hostname, params={})
         params.merge!("api_key" => consumer_key)
-        get build_query("/#{API_VERSION}/blog/#{params.delete("base-hostname")}/posts/#{params.delete("type")}",params)
+        get build_query("/#{API_VERSION}/blog/#{base_hostname}/posts/#{params.delete("type")}",params)
       end
 
-      def queue(params={})
-        check_required_params %w(base-hostname), params
-        get "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/posts/queue"
+      def queue(base_hostname, params={})
+        get "/#{API_VERSION}/blog/#{base_hostname}/posts/queue"
       end
 
-      def drafts(params={})
-        check_required_params %w(base-hostname), params
-        get "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/posts/draft"
+      def drafts(base_hostname, params={})
+        get "/#{API_VERSION}/blog/#{base_hostname}/posts/draft"
       end
       
-      def submissions(params={})
-        check_required_params %w(base-hostname), params
-        get "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/posts/submission"
+      def submissions(base_hostname, params={})
+        get "/#{API_VERSION}/blog/#{base_hostname}/posts/submission"
       end
       
-      def text(params={})
-        check_required_params %w(base-hostname body), params
+      def text(base_hostname, params={})
+        check_required_params %w(body), params
         params.merge!({"type" => "text"})
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def photo(params={})
-        check_required_params %w(base-hostname), params
+      def photo(base_hostname, params={})
         params.merge!({"type" => "photo"})
-        raise Tumblr::Error::MissingRequiredParameterError unless params.has_key?("source") || params.has_key?("data[]")
-        params["data[]"] = OAuth::Helper.escape(File.open(params["data[]"]).binmode.read) if params.has_key?("data[]")
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        raise Tumblr::Error::MissingRequiredParameterError.new("Missing \"source\" or \"data\" parameter") unless params.has_key?("source") || params.has_key?("data[]")
+        params["data[]"] = OAuth::Helper.escape(File.open(base_hostname, params["data[]"]).binmode.read) if params.has_key?("data[]")
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def quote(params={})
-        check_required_params %w(base-hostname quote), params
+      def quote(base_hostname, params={})
+        check_required_params %w(quote), params
         params.merge!({"type" => "quote"})
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def link(params={})
-        check_required_params %w(base-hostname url), params
+      def link(base_hostname, params={})
+        check_required_params %w(url), params
         params.merge!({"type" => "link"})
 
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def chat(params={})
-        check_required_params %w(base-hostname conversation), params
+      def chat(base_hostname, params={})
+        check_required_params %w(conversation), params
         params.merge!({"type" => "chat"})
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def audio(params={})
-        check_required_params %w(base-hostname), params
-        raise Tumblr::Error::MissingRequiredParameterError.new unless params.has_key?("external_url") || params.has_key?("data[]")
+      def audio(base_hostname, params={})
+        raise Tumblr::Error::MissingRequiredParameterError.new("Missing \"external_url\" or \"data\" parameter") unless params.has_key?("external_url") || params.has_key?("data[]")
         params.merge!({"type" => "audio"})
-        params["data[]"] = OAuth::Helper.escape(File.open(params["data[]"]).binmode.read) if params.has_key?("data[]")
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        params["data[]"] = OAuth::Helper.escape(File.open(base_hostname, params["data[]"]).binmode.read) if params.has_key?("data[]")
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def video(params={})
-        check_required_params %w(base-hostname), params
-        raise Tumblr::Error::MissingRequiredParameterError unless params.has_key?("embed") || params.has_key?("data[]")
+      def video(base_hostname, params={})
+        raise Tumblr::Error::MissingRequiredParameterError.new("Missing \"embed\" or \"data\" parameter") unless params.has_key?("embed") || params.has_key?("data[]")
         params.merge!({"type" => "video"})
-        params["data[]"] = OAuth::Helper.escape(File.open(params["data[]"]).binmode.read) if params.has_key?("data[]")
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post", params
+        params["data[]"] = OAuth::Helper.escape(File.open(base_hostname, params["data[]"]).binmode.read) if params.has_key?("data[]")
+        post "/#{API_VERSION}/blog/#{base_hostname}/post", params
       end
 
-      def edit(params={})
-        check_required_params %w(base-hostname id), params
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post/edit"
+      def edit(base_hostname, params={})
+        check_required_params %w(id), params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post/edit"
       end
 
-      def reblog(params={})
-        check_required_params %w(base-hostname id reblog_key), params
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post/reblog"
+      def reblog(base_hostname, params={})
+        check_required_params %w(id reblog_key), params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post/reblog"
       end
 
-      def delete_post(params={})
-        check_required_params %w(base-hostname id), params
-        post "/#{API_VERSION}/blog/#{params.delete("base-hostname")}/post/delete"
+      def delete_post(base_hostname, params={})
+        check_required_params %w( id), params
+        post "/#{API_VERSION}/blog/#{base_hostname}/post/delete"
       end
 
       private
--- spec/tumblr/client/blog_spec.rb
+++ spec/tumblr/client/blog_spec.rb
@@ -17,20 +17,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, /http:\/\/api\.tumblr\.com\/v2\/blog\/test\.tumblr\.com\/info*/).to_return(:status => 200, :body => fixture('blog_info.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.blog_info}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/info/}.returns('')
-        client.blog_info("base-hostname" => 'test.tumblr.com')
+        client.blog_info('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.blog_info("base-hostname" => 'test.tumblr.com') }
+      subject { client.blog_info('test.tumblr.com') }
       it { should_not be_nil } 
       its("response.blog.name"){should eq("david")}
       its("response.blog.title"){should eq("David's Log")}
@@ -44,20 +40,16 @@ describe Tumblr::Client::Blog do
                                                                                                       :headers => {"Location" => "http://30.media.tumblr.com/avatar_61d09ab16eb4_16.png", "Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.avatar}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/avatar/}.returns('')
-        client.avatar("base-hostname" => 'test.tumblr.com')
+        client.avatar('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.avatar("base-hostname" => 'test.tumblr.com') }
+      subject { client.avatar('test.tumblr.com') }
       it { should_not be_nil } 
       its("meta.status"){ should eq(301) }
       its("response.avatar_url"){ should eq("http://30.media.tumblr.com/avatar_61d09ab16eb4_16.png") }
@@ -70,20 +62,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, /http:\/\/api\.tumblr\.com\/v2\/blog\/test\.tumblr\.com\/followers*/).to_return(:status => 200, :body => fixture('followers.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.followers}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-    
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/followers/}.returns('')
-        client.followers("base-hostname" => 'test.tumblr.com')
+        client.followers('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.followers("base-hostname" => 'test.tumblr.com') }
+      subject { client.followers('test.tumblr.com') }
       it { should_not be_nil } 
       its("meta.status"){ should eq(200) }
       its("response.users"){ should be_an_instance_of(Array) }
@@ -98,20 +86,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, /http:\/\/api\.tumblr\.com\/v2\/blog\/test\.tumblr\.com\/posts*/).to_return(:status => 200, :body => fixture('posts.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.posts}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/posts/}.returns('')
-        client.posts("base-hostname" => 'test.tumblr.com')
+        client.posts('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.posts("base-hostname" => 'test.tumblr.com') }
+      subject { client.posts('test.tumblr.com') }
       it { should_not be_nil } 
       its('response.posts.size') { should eq(2)}
     end
@@ -123,20 +107,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, "http://api.tumblr.com/v2/blog/test.tumblr.com/posts/queue").to_return(:status => 200, :body => fixture('posts.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.queue}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/posts\/queue/}.returns('')
-        client.queue("base-hostname" => 'test.tumblr.com')
+        client.queue('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.queue("base-hostname" => 'test.tumblr.com') }
+      subject { client.queue('test.tumblr.com') }
       it { should_not be_nil } 
       its('response.posts.size') { should eq(2)}
     end
@@ -148,20 +128,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, "http://api.tumblr.com/v2/blog/test.tumblr.com/posts/draft").to_return(:status => 200, :body => fixture('posts.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.drafts}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/posts\/draft/}.returns('')
-        client.drafts("base-hostname" => 'test.tumblr.com')
+        client.drafts('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.drafts("base-hostname" => 'test.tumblr.com') }
+      subject { client.drafts('test.tumblr.com') }
       it { should_not be_nil } 
       its('response.posts.size') { should eq(2)}
     end
@@ -173,20 +149,16 @@ describe Tumblr::Client::Blog do
       stub_request(:get, "http://api.tumblr.com/v2/blog/test.tumblr.com/posts/submission").to_return(:status => 200, :body => fixture('posts.json'), :headers => {"Content-Type" => "application/json"})
     end
 
-    context "missing required params" do
-      it{ expect {client.submissions}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
-    end
-
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:get).returns('')
         client.expects(:get).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/posts\/submission/}.returns('')
-        client.submissions("base-hostname" => 'test.tumblr.com')
+        client.submissions('test.tumblr.com')
       end
     end
 
     context "required params present" do
-      subject { client.submissions("base-hostname" => 'test.tumblr.com') }
+      subject { client.submissions('test.tumblr.com') }
       it { should_not be_nil } 
       its('response.posts.size') { should eq(2)}
     end
@@ -195,14 +167,14 @@ describe Tumblr::Client::Blog do
   describe "#text" do
     
     context "missing required params" do
-      it{ expect {client.text}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.text('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.text("base-hostname" => 'test.tumblr.com', "body" => "test")
+        client.text('test.tumblr.com', "body" => "test")
       end
     end
 
@@ -211,14 +183,14 @@ describe Tumblr::Client::Blog do
   describe "#photo" do
     
     context "missing required params" do
-      it{ expect {client.photo}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.photo('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.photo("base-hostname" => 'test.tumblr.com', "source" => "http://test.com/image.jpg")
+        client.photo('test.tumblr.com', "source" => "http://test.com/image.jpg")
       end
     end
 
@@ -227,14 +199,14 @@ describe Tumblr::Client::Blog do
   describe "#quote" do
   
     context "missing required params" do
-      it{ expect {client.quote}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.quote('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.quote("base-hostname" => 'test.tumblr.com', "quote" => "Test")
+        client.quote('test.tumblr.com', "quote" => "Test")
       end
     end
   end
@@ -242,14 +214,14 @@ describe Tumblr::Client::Blog do
   describe "#link" do
 
     context "missing required params" do
-      it{ expect {client.link}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.link('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.link("base-hostname" => 'test.tumblr.com', "url" => "Test")
+        client.link('test.tumblr.com', "url" => "Test")
       end
     end
 
@@ -258,14 +230,14 @@ describe Tumblr::Client::Blog do
   describe "#chat" do
     
     context "missing required params" do
-      it{ expect {client.chat}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.chat('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.chat("base-hostname" => 'test.tumblr.com', "conversation" => "Test")
+        client.chat('test.tumblr.com', "conversation" => "Test")
       end
     end
   end
@@ -273,14 +245,14 @@ describe Tumblr::Client::Blog do
   describe "#audio" do
   
     context "missing required params" do
-      it{ expect {client.audio}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.audio('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.audio("base-hostname" => 'test.tumblr.com', "external_url" => "http://test.com/test.mp3")
+        client.audio('test.tumblr.com', "external_url" => "http://test.com/test.mp3")
       end
     end
 
@@ -289,14 +261,14 @@ describe Tumblr::Client::Blog do
   describe "#video" do
 
     context "missing required params" do
-      it{ expect {client.video}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.video('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post/}.returns('')
-        client.video("base-hostname" => 'test.tumblr.com', "embed" => "embed")
+        client.video('test.tumblr.com', "embed" => "embed")
       end
     end
 
@@ -305,14 +277,14 @@ describe Tumblr::Client::Blog do
   describe "#edit" do
     
     context "missing required params" do
-      it{ expect {client.edit}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.edit('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post\/edit/}.returns('')
-        client.edit("base-hostname" => 'test.tumblr.com', "id" => "123")
+        client.edit('test.tumblr.com', "id" => "123")
       end
     end
     
@@ -321,14 +293,14 @@ describe Tumblr::Client::Blog do
   describe "#reblog" do
   
     context "missing required params" do
-      it{ expect {client.reblog}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.reblog('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post\/reblog/}.returns('')
-        client.reblog("base-hostname" => 'test.tumblr.com', "reblog_key" => "123", "id" => "456")
+        client.reblog('test.tumblr.com', "reblog_key" => "123", "id" => "456")
       end
     end
 
@@ -337,14 +309,14 @@ describe Tumblr::Client::Blog do
   describe "#delete_post" do
 
     context "missing required params" do
-      it{ expect {client.delete_post}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
+      it{ expect {client.delete_post('test.tumblr.com')}.to raise_error(Tumblr::Error::MissingRequiredParameterError)}
     end
 
     context "making request" do
       it "should make request to correct url" do
         client.stubs(:post).returns('')
         client.expects(:post).with{ |arg| arg =~ /\/blog\/test\.tumblr\.com\/post\/delete/}.returns('')
-        client.delete_post("base-hostname" => 'test.tumblr.com', "id" => "456")
+        client.delete_post('test.tumblr.com', "id" => "456")
       end
     end
 
